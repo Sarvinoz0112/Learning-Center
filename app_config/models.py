0dffec31d91e2a25273b39e2649807d1731518c3
Sarvinoz0112 <maxmadaliyevasarvinoz@gmail.com>
2025-02-28 14:21:54 +0500

    first commit

---
+++ app_config/models.py
@@ -0,0 +1,109 @@
+from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin, Group, Permission
+from django.core.validators import RegexValidator
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, phone, password=None, **extra_fields):
+        if not phone:
+            raise ValueError('Telefon raqami kiritilishi shart')
+        user = self.model(phone=phone, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, phone, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser is_staff=True bo‘lishi kerak.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser is_superuser=True bo‘lishi kerak.')
+        return self.create_user(phone, password, **extra_fields)
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    phone_regex = RegexValidator(regex=r'^\+998\d{9}$',
+                                 message="Phone number must be entered in the format: '+998901234567'.")
+    phone = models.CharField(validators=[phone_regex], max_length=13, unique=True)
+    full_name = models.CharField(max_length=50, null=True, blank=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
+    is_student = models.BooleanField(default=False)
+    is_teacher = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    username = None
+
+    groups = models.ManyToManyField(Group, related_name="custom_user_groups", blank=True)
+    user_permissions = models.ManyToManyField(Permission, related_name="custom_user_permissions", blank=True)
+
+    USERNAME_FIELD = 'phone'
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.phone
+
+    def has_perm(self, perm, obj=None):
+        return self.is_admin
+
+    def has_module_perms(self, app_label):
+        return self.is_admin
+
+
+class Course(models.Model):
+    title = models.CharField(max_length=50)
+    descriptions = models.CharField(max_length=500, null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Branch(models.Model):
+    title = models.CharField(max_length=50)
+    is_active = models.BooleanField(default=True)
+    descriptions = models.CharField(max_length=500, null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Teacher(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    branch = models.ManyToManyField(Branch, related_name='teachers')
+    course = models.ManyToManyField(Course, related_name='teachers')
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    descriptions = models.CharField(max_length=500, blank=True, null=True)
+
+    def __str__(self):
+        return self.user.phone
+
+
+class Student(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    group = models.ManyToManyField('StudyGroup', related_name='students')
+    course = models.ManyToManyField(Course, related_name='students')
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    descriptions = models.CharField(max_length=500, blank=True, null=True)
+
+    def __str__(self):
+        return self.user.phone
+
+class StudyGroup(models.Model):
+    title = models.CharField(max_length=50, unique=True)
+    course = models.ForeignKey(Course, on_delete=models.RESTRICT)
+    teacher = models.ManyToManyField(Teacher, related_name='groups')
+    created = models.DateField(auto_now_add=True)
+    updated = models.DateField(auto_now=True)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    price = models.CharField(max_length=15, blank=True, null=True)
+    descriptions = models.CharField(max_length=500, blank=True, null=True)
+
+    def __str__(self):
+        return self.title
